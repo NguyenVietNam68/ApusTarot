a02ec5ba027081a9790e1a25ea1ef6cd6f80adb1
NguyenVietNam68 <namzoe2021@gmail.com>
2022-04-13 19:42:02 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//route
+app.get('/', (req, res) => res.send('hello world!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
